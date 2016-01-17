06f1363d8ac3b059620aecde41ae2f5ec62a0c35
roman <roman.czerkies@gmail.com>
2016-01-17 21:27:11 +0100

    Fix(ele): Racine site
    Racine site dynamique
    
    Issue #1

--- ele/doc.php
+++ ele/doc.php
@@ -1,8 +1,12 @@
 <?php
+
+// Racine site
+define('RACINE', '/espaceetstyle/');
+
 //Connexion SQL
 ?>
 <!DOCTYPE html>
 	<html lang="fr">
 		<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" type="text/css" href="/style.css">
+		<link rel="stylesheet" type="text/css" href="<?= RACINE; ?>style.css">
--- ele/head_menu.php
+++ ele/head_menu.php
@@ -1,8 +1,8 @@
 </head>
 	<body>
-		<a href="/">
+		<a href="<?= RACINE; ?>">
 		<div class="logo">
-			<img src="/Ban1440jpg.jpg" alt="Espace et Style - Spécialisés dans l’agencement d’espace">
+			<img src="<?= RACINE; ?>Ban1440jpg.jpg" alt="Espace et Style - Spécialisés dans l’agencement d’espace">
 			<?php
 			if ($menu === 'accueil') {
 				echo '<p class="signature_acc">Agencement - Décoration - Rénovation</p>';
@@ -19,21 +19,21 @@
 		</div>
 		<nav class="menu">
 			<ul>
-				<li<?php if($menu === 'accueil') {echo ' class="ac"';} ?>><a href="/">Accueil</a></li>
-				<li<?php if($menu === 'quisommesnous') {echo ' class="ac"';} ?>><a href="/qui-sommes-nous/">Qui sommes nous ?</a></li>
-				<li><a <?php if($menu === 'votreprojet') {echo ' class="ac"';} ?> href="#">Votre projet</a>
+				<li<?php if($menu === 'accueil') {echo ' class="ac"';} ?>><a href="<?= RACINE; ?>">Accueil</a></li>
+				<li<?php if($menu === 'quisommesnous') {echo ' class="ac"';} ?>><a href="<?= RACINE; ?>qui-sommes-nous/">Qui sommes nous ?</a></li>
+				<li><a <?php if($menu === 'votreprojet') {echo ' class="ac"';} ?> href="<?= RACINE; ?>votre-projet/">Votre projet</a>
 				<ul>
-					<li><a <?php if($s_menu === 'nouveaux-locaux') {echo ' class="ac"';} ?> href="/votre-projet/nouveaux-locaux/">Emménagement dans vos nouveaux locaux</a></li>
-					<li><a <?php if($s_menu === 'point-de-vente') {echo ' class="ac"';} ?> href="/votre-projet/point-de-vente/">Agrandissement de votre point de vente</a></li>
-					<li><a <?php if($s_menu === 'charte-graphite') {echo ' class="ac"';} ?> href="/votre-projet/charte-graphite/">Évolution de votre charte graphite</a></li>
-					<li><a <?php if($s_menu === 'espace-de-vente') {echo ' class="ac"';} ?> href="/votre-projet/espace-de-vente/">Moderniser votre espace de vente</a></li>
-					<li><a <?php if($s_menu === 'agencement-de-magasin') {echo ' class="ac"';} ?> href="/votre-projet/agencement-de-magasin/">Agencement de magasin</a></li>
-					<li><a <?php if($s_menu === 'renovation-de-magasin') {echo ' class="ac"';} ?> href="/votre-projet/renovation-de-magasin/">Rénovation de magasin</a></li>
+					<li><a <?php if(isset($s_menu) && $s_menu === 'nouveaux-locaux') {echo ' class="ac"';} ?> href="<?= RACINE; ?>votre-projet/nouveaux-locaux/">Emménagement dans vos nouveaux locaux</a></li>
+					<li><a <?php if(isset($s_menu) && $s_menu === 'point-de-vente') {echo ' class="ac"';} ?> href="<?= RACINE; ?>votre-projet/point-de-vente/">Agrandissement de votre point de vente</a></li>
+					<li><a <?php if(isset($s_menu) && $s_menu === 'charte-graphite') {echo ' class="ac"';} ?> href="<?= RACINE; ?>votre-projet/charte-graphite/">Évolution de votre charte graphite</a></li>
+					<li><a <?php if(isset($s_menu) && $s_menu === 'espace-de-vente') {echo ' class="ac"';} ?> href="<?= RACINE; ?>votre-projet/espace-de-vente/">Moderniser votre espace de vente</a></li>
+					<li><a <?php if(isset($s_menu) && $s_menu === 'agencement-de-magasin') {echo ' class="ac"';} ?> href="<?= RACINE; ?>votre-projet/agencement-de-magasin/">Agencement de magasin</a></li>
+					<li><a <?php if(isset($s_menu) && $s_menu === 'renovation-de-magasin') {echo ' class="ac"';} ?> href="<?= RACINE; ?>votre-projet/renovation-de-magasin/">Rénovation de magasin</a></li>
 				</ul>
 				</li>
-				<li<?php if($menu === 'notre-demarche') {echo ' class="ac"';} ?>><a href="/notre-demarche/">Notre démarche</a></li>
-				<li<?php if($menu === 'nosrealisations') {echo ' class="ac"';} ?>><a href="/nos-realisations/">Nos réalisations</a></li>
-				<li<?php if($menu === 'contact') {echo ' class="ac"';} ?>><a href="/contact-devis/">Contact</a></li>
+				<li<?php if($menu === 'notre-demarche') {echo ' class="ac"';} ?>><a href="<?= RACINE; ?>notre-demarche/">Notre démarche</a></li>
+				<li<?php if($menu === 'nosrealisations') {echo ' class="ac"';} ?>><a href="<?= RACINE; ?>nos-realisations/">Nos réalisations</a></li>
+				<li<?php if($menu === 'contact') {echo ' class="ac"';} ?>><a href="<?= RACINE; ?>contact-devis/">Contact</a></li>
 			</ul>
 		</nav>
 		<div class="prin">
