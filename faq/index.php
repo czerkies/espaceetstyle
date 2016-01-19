e9ecb6af9bde36218985bb5f72221f882bc02f51
czerkies <roman.czerkies@gmail.com>
2016-01-19 07:49:13 +0100

    Feat(faq): Nouvelle page
    FAQ, menu et cont.
    
    Issue #3

---
+++ faq/index.php
@@ -0,0 +1,15 @@
+<?php
+$menu = 'faq';
+include('../ele/doc.php');
+?>
+		<title>Agencement intérieur magasin - Espace et Style</title>
+		<meta name="description" content="Entourés d’une équipe de cinq designers d’espace spécialisés dans l’agencement, nous vous accompagnons pour tous vos projets.">
+		<?php include('../ele/head_menu.php'); ?>
+		<div class="bloc_100 bloc_faq">
+			<div class="bloc_0">
+				<h2>FAQ - Espace et style</h2>
+        <?php include_once 'menu_faq.php'; ?>
+			</div>
+		</div>
+	</div>
+	<?php include('../ele/foo.php'); ?>
