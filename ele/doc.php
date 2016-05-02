a14836dbe4fc571d0f351e3963f4a96dcd8d86ed
czerkies <roman.czerkies@gmail.com>
2016-05-02 09:18:01 +0200

    Feat(/): Amélioration CSS et IMG
    CSS renomé version 4.
    Toutes images en 72ppp.
    
    Issue #10 #14

--- ele/doc.php
+++ ele/doc.php
@@ -3,7 +3,7 @@
 	<html lang="fr">
 		<head>
 			<meta charset="UTF-8">
-			<link rel="stylesheet" type="text/css" href="<?= RACINE; ?>style320.css">
+			<link rel="stylesheet" type="text/css" href="<?= RACINE; ?>style400.css">
 			<script>
 			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
