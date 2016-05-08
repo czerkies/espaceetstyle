ca302779997a162376a579a9142ff40a5004418c
roman <roman.czerkies@gmail.com>
2016-05-08 22:00:18 +0200

    Fix(style/html): MAJ animation responsive
    MAJ animation pour responsive.
    
    Issue #10

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ include('ele/doc.php');
 		<meta name="description" content="Spécialisés dans l’agencement d’espace, nous sommes à votre disposition pour étudier tout projet de rénovation, décoration ou agencement.">
 	<?php include('ele/head_menu.php'); ?>
 			<div class="car car_acc">
-				<a href="<?= RACINE; ?>qui-sommes-nous/">
-					<div class="accueil_center">
-						Agencer votre espace, le faire vivre pour révéler votre potentiel...
-					</div>
-				</a>
 				<div class="citation">
 					<p>"Le fer se rouille, faute de s'en servir,<br>
 					 l'eau stagnante perd de sa pureté et se glace par le froid.<br>
 					 De même, l'inaction sape la vigueur de l'esprit."<br>
 					<span>Léonard De Vinci</span></p>
 				</div>
+				<a href="<?= RACINE; ?>qui-sommes-nous/">
+					<div class="accueil_center">
+						Agencer votre espace, le faire vivre pour révéler votre potentiel...
+					</div>
+				</a>
 				<div class="car_acc_back">
 					<!-- 1 -->
 					<img src="<?= RACINE; ?>img/BUREAU-BOULOGNE.jpg" alt="Espace et style - Bureau de boulogne">
